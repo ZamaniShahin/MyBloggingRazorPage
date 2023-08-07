4e3e5a97291aff0f5449a3485a029e92f13b023f
ZamaniShahin <sh.zamani1581@gmail.com>
2023-08-07 17:26:11 +0330

    Add project files.

---
+++ MyBloggingRazorPage/Pages/CreateNewArticle.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MyBloggingRazorPage.Pages
+{
+    public class CreateNewArticleModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
